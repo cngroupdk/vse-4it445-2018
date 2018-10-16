9415787e6e0566ce57d59722b9d9e4a0b9e94266
Tomáš Horáček <tomas.horacek@gmail.com>
2018-10-16 18:07:38 +0200

    practical-05-1615-end

--- frontend/src/components/pages/ProductListPage.js
+++ frontend/src/components/pages/ProductListPage.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import { PRODUCTS } from '../../mock-data';
 import { ProductListTemplate } from '../templates/ProductListTemplate';
+import { startFetchProducts } from '../../services/ProductList/actions';
+import {
+  getProducts,
+  getIsLoaded,
+  getIsLoading,
+  getIsError,
+  getError,
+} from '../../services/ProductList/reducer';
 
 class ProductListPageRaw extends Component {
+  componentDidMount() {
+    const { startFetchProducts, isLoaded } = this.props;
+    if (isLoaded) {
+      return;
+    }
+
+    startFetchProducts();
+  }
+
   render() {
-    const products = PRODUCTS;
-    const isLoading = false;
-    const isLoaded = true;
-    const isError = false;
-    const error = null;
+    const { products, isLoading, isLoaded, isError, error } = this.props;
 
     return (
       <ProductListTemplate
@@ -23,4 +37,23 @@ class ProductListPageRaw extends Component {
   }
 }
 
-export const ProductListPage = ProductListPageRaw;
+const mapStateToProps = storeState => {
+  const { productList } = storeState;
+  return {
+    isLoading: getIsLoading(productList),
+    isLoaded: getIsLoaded(productList),
+    isError: getIsError(productList),
+    products: getProducts(productList),
+    error: getError(productList),
+  };
+};
+
+// actions
+const mapDispatchToProps = {
+  startFetchProducts,
+};
+
+export const ProductListPage = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ProductListPageRaw);
